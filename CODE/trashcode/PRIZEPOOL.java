d8a7711c091fb35d5e7bac3152a0598761fa2572
Soumya <soumyamukherjee@mail.com>
2022-07-16 13:15:59 +0530

    added few files

--- CODE/trashcode/PRIZEPOOL.java
+++ CODE/trashcode/PRIZEPOOL.java
@@ -1,16 +1,15 @@
 import java.lang.*;
 import java.util.*;
+import java.io.*;
 class PRIZEPOOL{
   public static void main(String args[]){
     BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
     int t =Integer.parseInt(br.readLine());
-    for(int i=0;i<t;i++){
-      int a=Integer.parseInt(br.readLine());
-      int b=Integer.parseInt(br.readLIne());
-      int sum = (a*10) + (b*90);
+    for(int i=0;i<t;i++) {
+      int a = Integer.parseInt(br.readLine());
+      int b = Integer.parseInt(br.readLIne());
+      int sum = (a * 10) + (b * 90);
       System.out.println(sum);
     }
-
-
   }
 }
